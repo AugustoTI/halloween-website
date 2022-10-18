1a809de1b11cfe3bbce0c28751e7ce518d89368c
AugustoTI <augustocesarti2021@gmail.com>
2022-10-18 12:47:50 -0300

    creating the mobile header

---
+++ src/menu.ts
@@ -0,0 +1,28 @@
+const navMenu = document.querySelector('#nav-menu'),
+  navToggle = document.querySelector('#nav-toggle'),
+  navClose = document.querySelector('#nav-close')
+
+// MENU SHOW
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu?.classList.add('show-menu')
+  })
+}
+
+// MENU HIDDEN
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu?.classList.remove('show-menu')
+  })
+}
+
+// REMOVE MENU MOBILE WHEN CLICKING ON LINK
+const navLinks = document.querySelectorAll('.nav_link')
+
+const linkAction = () => {
+  navMenu?.classList.remove('show-menu')
+}
+
+navLinks.forEach((link) => link.addEventListener('click', linkAction))
+
+export {}
